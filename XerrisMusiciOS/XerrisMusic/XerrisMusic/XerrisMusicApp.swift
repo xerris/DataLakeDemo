e446eee519ea4e15227ab13655b19012385ca894
Jason Wiker <jason.wiker@gmail.com>
2020-12-26 11:54:37 -0700

    Add template for app

---
+++ XerrisMusiciOS/XerrisMusic/XerrisMusic/XerrisMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  XerrisMusicApp.swift
+//  XerrisMusic
+//
+//  Created by Jason Wiker on 2020-12-26.
+//
+
+import SwiftUI
+
+@main
+struct XerrisMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
