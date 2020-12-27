b01d8b0d9a91dd87951459a381efc493e0ed15a7
Jason Wiker <jason.wiker@gmail.com>
2020-12-27 16:09:32 -0700

    update content view

--- XerrisMusiciOS/XerrisMusic/XerrisMusic/ContentView.swift
+++ XerrisMusiciOS/XerrisMusic/XerrisMusic/ContentView.swift
@@ -10,44 +10,109 @@ import Alamofire
 import AWSPinpoint
 import Amplify
 
-struct Track: Decodable {
-    let mbid: String
+struct TrackImage: Codable {
+    let size: String
+    let text: String
+    enum CodingKeys: String, CodingKey{
+           case text = "#text"
+            case size = "size"
+       }
 }
-struct RecentTracks: Decodable {
+
+struct TrackDate: Codable {
+    let uts: String
+    let text: String
+    enum CodingKeys: String, CodingKey{
+           case text = "#text"
+            case uts = "uts"
+       }
+}
+
+struct TrackDetail: Codable {
+    let mbid: String
+    let text: String
+    enum CodingKeys: String, CodingKey{
+           case text = "#text"
+            case mbid = "mbid"
+       }
+}
+
+struct Track: Codable {
+    let mbid: String
+    let name: String
+    let url: String
+    let artist: TrackDetail
+    let album: TrackDetail
+    let image: [TrackImage]
+    let date: TrackDate
+    
+}
+struct RecentTracks: Codable {
     let track: [Track]
 }
-struct Response: Decodable {
+
+struct TrackResponse: Codable {
     let recenttracks: RecentTracks
 }
+struct TagDetail: Codable {
+    let name: String
+    let url: String
+}
+struct TagDetails: Codable {
+    let tag: [TagDetail]
+}
+struct AlbumDetails: Codable {
+    let listeners: String
+    let tags: TagDetails
+}
+struct AlbumResponse: Codable {
+    let album: AlbumDetails
+}
+
 struct ContentView: View {
     @State var username: String = ""
     
     func callGet() {
-
-        AF.request("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=megaranger&api_key=f2bb8310d17edb41045aba6362611f80&format=json&page=1",
-                   method: .get).responseDecodable(of: Response.self) { response in
+        AF.request("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=megaranger&api_key=f2bb8310d17edb41045aba6362611f80&format=json&page=170&limit=200",
+                   method: .get).responseDecodable(of: TrackResponse.self) { response in
                     
-            debugPrint("Response: \(response)")
+            //debugPrint("Response: \(response)")
+            let tracks: [Track] = (response.value?.recenttracks.track)!
+            for track in tracks {
+                callGetAlbum(track: track)
+            }
         }
     }
     
-    func callPost() {
-//        let firehoseRecorder = AWSFirehoseRecorder.default()
-//        let yourData = "Test_data".data(using: .utf8)
-//        firehoseRecorder.saveRecord(yourData, streamName: "YourStreamName")
-//        firehoseRecorder.submitAllRecords()
+    func callGetAlbum(track: Track) {
+        let url = ("https://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=f2bb8310d17edb41045aba6362611f80&format=json&mbid=" + track.album.mbid)
+      
+        AF.request(url, method: .post).responseDecodable(of: AlbumResponse.self) { response in
+
+            //debugPrint("Response: \(response)")
+            let listeners: String = response.value?.album.listeners ?? "?"
+            var properties: AnalyticsProperties = [
+                "artist": track.artist.text,
+                "album": track.album.text,
+                "playTime": track.date.uts,
+                "trackname": track.name
+            ]
+            if listeners != "?"{
+                properties["listeners"] = listeners
+            }
+            var tagVal = 1
+            let tags: [TagDetail] = response.value?.album.tags.tag ?? []
+            for tag in tags {
+                properties["tag" + String(tagVal)] = tag.name
+                tagVal += 1
+            }
+            print(properties)
+            let event = BasicAnalyticsEvent(name: "SongListen", properties: properties)
+            Amplify.Analytics.record(event: event)
+        }
+        
     }
-    
-    func recordEvents() {
-        let properties: AnalyticsProperties = [
-            "eventPropertyStringKey": "eventPropertyStringValue",
-            "eventPropertyIntKey": 123,
-            "eventPropertyDoubleKey": 12.34,
-            "eventPropertyBoolKey": true
-        ]
-        let event = BasicAnalyticsEvent(name: "eventName", properties: properties)
-        Amplify.Analytics.record(event: event)
-    }
+
     
     var body: some View {
            VStack {
@@ -65,8 +130,6 @@ struct ContentView: View {
                 }.padding()
                 Button(action: {
                             self.callGet()
-                            self.callPost()
-                    self.recordEvents()
                            }) {
                                Text("Play")
                            }
